fd9f9c6f50fcd587098959a6cfb0934f97c22d22
RhodesKeegan <rhodesk@msoe.edu>
2022-05-24 19:03:02 -0500

    Messing around with shaders

---
+++ OpenGL project/OpenGL project/include/main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void viewPortHandler(GLFWwindow* window, int width, int height);
+void keyHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
+void makeTriangles();
--- OpenGL project/OpenGL project/src/main.cpp
+++ OpenGL project/OpenGL project/src/main.cpp
@@ -1,23 +1,23 @@
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+#include <GLM/include/vec3.hpp>
 #include <iostream>
+#include <fstream>
+
+#include "main.h"
 
 static int WIDTH = 1920;
 static int HEIGHT = 1080;
 
-void viewPortHandler(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
+static std::string readFile(const char* path);
 
-void keyHandler(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-		std::cout << "You pressed the escape key buddy" << std::endl;
-	}
-}
+using namespace std;
 
-int main(void) {
+int main(int argc, char* argv[]) {
 	glfwInit();
 
+	//Setting the window to fullscreen
+	glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
 
 	if (!window) {
@@ -25,6 +25,7 @@ int main(void) {
 		glfwTerminate();
 
 	}
+	
 
 	glfwMakeContextCurrent(window);
 
@@ -33,10 +34,14 @@ int main(void) {
 		EXIT_FAILURE;
 	}
 
-
 	glViewport(0, 0, WIDTH, HEIGHT);
+
+
+	//Setting up the callback functions
 	glfwSetFramebufferSizeCallback(window, viewPortHandler);
 	glfwSetKeyCallback(window, keyHandler);
+
+
 	while (!glfwWindowShouldClose(window)) {
 
 		//Sets it a light red or something
@@ -50,4 +55,59 @@ int main(void) {
 
 	glfwTerminate();
 	EXIT_SUCCESS;
+}
+
+void keyHandler(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		cout << "You pressed the escape key buddy" << endl;
+	}
+}
+
+void viewPortHandler(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void makeTriangles() {
+	glm::vec3 vertices[] = {
+		glm::vec3(-0.5f, -0.5f, 0.0f),
+		glm::vec3(0.5f, -0.5f, 0.0f),
+		glm::vec3(0.0f,  0.5f, 0.0f)
+	};
+
+
+	//Doing the binding stuff here
+	unsigned int vboID;
+
+	glGenBuffers(1, &vboID);
+	glBindBuffer(GL_ARRAY_BUFFER, vboID);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	//Making the shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	
+	string sourceCode = readFile("../resources/shaders/vertex/triangle.glsl");
+
+
+	const GLchar* triangleSource = sourceCode.c_str();
+	glShaderSource(vertexShader, 1, &triangleSource, NULL);
+	glCompileShader(vertexShader);
+
+
+}
+
+static string readFile(const char* path) {
+	string resultingString;
+
+	ifstream in(path, std::ios::in | std::ios::binary);
+	if (in){
+		in.seekg(0, std::ios::end);
+		resultingString.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&resultingString[0], resultingString.size());
+		in.close();
+	}
+
+	return resultingString;
 }
